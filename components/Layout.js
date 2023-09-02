14c9da91c106ce9e974e9d970a1d0c5bbb10c57a
takazo3 <info@re-zero-jp.com>
2023-09-03 01:40:01 +0900

    full

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "./layout.module.css"
+import utilsStyle from "../styles/utils.module.css"
+
+const name = "Pave Up Blog";
+export const siteTitle = "Pave Up Blog";
+
+function Layout({children}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilsStyle.borderCircle} />
+        <h1 className={utilsStyle.heading2Xl}>{name}</h1>
+      </header>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
